868f8a33322a77bf7a43f5ee79303ab4af72275f
nayan-bansal <nayan.7991@gmail.com>
2020-09-28 22:48:57 +0530

    Added: Add Contact Option

--- Address.java
+++ Address.java
@@ -1,14 +1,62 @@
-  
-package address;
+package AddressBook;
 
+import java.util.*;
 
+public class Address {
 
-public class AddressBookMain{
 	
-	public static void main(String[] args) {	
+	public static void main (String[] args) {
 		
-		//Printing welcome message
-		System.out.println("Welcome To Address Book Program");
+		//Printing Welcom Message
+		System.out.println("Welcome to Address Book Developement System");
+		
+		Information add = new Information();
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Press 1 to Add Contact");
+		System.out.println("Press 2 to Update a Contact");
+		System.out.println("Press 3 to Delete a Contact");
+		System.out.println("Press 4 to Exit");
+		
+		int choice = scan.nextInt();
+		int count = 0;
+		
+		switch(choice) {
+		case 1:
+			Directory data = new Directory();
+			System.out.println("Enter First Name: ");
+			data.setFirst_Name(scan.next());
+			System.out.println("Enter Last Name: ");
+			data.setLast_Name(scan.next());
+			System.out.println("Enter Address: ");
+			data.setLast_Name(scan.next());
+			System.out.println("Enter City ");
+			data.setLast_Name(scan.next());
+			System.out.println("Enter State ");
+			data.setLast_Name(scan.next());
+			System.out.println("Enter Zip Code: ");
+			data.setLast_Name(scan.next());
+			System.out.println("Enter Phone Number: ");
+			data.setLast_Name(scan.next());
+			System.out.println("Enter Email Address: ");
+			data.setLast_Name(scan.next());
+			add.addPerson(data);
+			count++;
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			if(count == 0)
+				System.out.println("The List is empty");
+			
+			System.out.println("Thank you for Visiting the Address Book");
+			break;
+		}
+		
+	
 		
 	}
 }
