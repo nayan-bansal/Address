2b16fdb9c99404b651e4cda31f7a3dbf8cefa73b
nayan-bansal <nayan.7991@gmail.com>
2020-09-29 02:57:49 +0530

    Added: Modify the data in Address Book

--- Information.java
+++ Information.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Information {
 
+	Scanner scan = new Scanner(System.in);
 	
 	private ArrayList<Directory> person = new ArrayList<Directory>();
 	
@@ -19,6 +20,51 @@ public class Information {
 		person.add(data);
 	}
 	
-	
+	public void modifyPerson(String name) {
+		for (int i=0; i< person.size();i++) {
+			
+			Directory data = (Directory)person.get(i);
+			if(name.equals(data.getFirst_Name())) {
+				System.out.println("Which Details to modify? Choose\n1. First Name\n2. Last Name\n3. Address\n4. City\n5. State\n6. Zip Code\n7. Phone Number\n8. EmailId\n9. Exit");
+			int choice  = scan.nextInt();	
+			switch(choice) {
+			
+			case 1:
+				System.out.println("Enter new First Name");
+				data.setFirst_Name(scan.next());
+     		   break;
+     	   	
+			case 2: 
+     	   		System.out.println("Enter new Last Name");
+    				data.setLast_Name(scan.next());
+    				break;
+     	   	case 3: 
+     	   		System.out.println("Enter new Address");
+    				data.setAddress(scan.next());
+    				break;
+     	   	case 4: 
+     	   		System.out.println("Enter new City");
+    				data.setCity_Name(scan.next());
+    				break;
+     	   	case 5:
+     	   		System.out.println("Enter new State");
+    				data.setState(scan.next());
+    				break;
+     	   	case 6:
+     	   		System.out.println("Enter new Zip Code");
+     	   		data.setZip_Number(scan.nextInt());
+     	   		break;
+     	   	case 7:
+     	   		System.out.println("Enter new Phone Number");
+    				data.setPhone_Number(scan.nextLong());
+    				break;
+     	   	case 8:
+     	   		System.out.println("Enter new Email");
+    				data.setEmail_Id(scan.next());
+    				break;
+			}
+			}
+		}
+	}
 	
 } 
