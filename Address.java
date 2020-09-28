5288e1e4c4902a6dd9997c20e8bcb5a94794af94
nayan-bansal <nayan.7991@gmail.com>
2020-09-28 12:25:23 +0530

    Welcome Message

---
+++ Address.java
@@ -0,0 +1,14 @@
+  
+package address;
+
+
+
+public class AddressBookMain{
+	
+	public static void main(String[] args) {	
+		
+		//Printing welcome message
+		System.out.println("Welcome To Address Book Program");
+		
+	}
+}
