df45514f151eda6f87dcd6b2ce2c593504594e2d
nayan-bansal <nayan.7991@gmail.com>
2020-10-01 01:55:36 +0530

    Added: Address Book with multiple contacts

--- Information.java
+++ Information.java
@@ -4,81 +4,87 @@ import java.util.*;
 
 public class Information {
 
-	Scanner scan = new Scanner(System.in);
+	Scanner obj=new Scanner(System.in);
+	// Array list to store contact person details
 	
-	private ArrayList<Directory> person = new ArrayList<Directory>();
-	
-	public ArrayList<Directory> getPerson (){
-		return person;	
-	}
-	
-	public void setPerson(ArrayList<Directory> person) {
+		
+	private ArrayList<PersonInfo> person=new ArrayList<PersonInfo>();
+		
+	public void setPerson(ArrayList<PersonInfo> person) {
 		this.person=person;
 	}
+	
+	public ArrayList<PersonInfo> getPerson() {
+		return person;
+	}
+	
+	//Method to add contact person details
+	public void addPerson(PersonInfo pobj ) {
+		person.add(pobj);
+	}
+	
+	//Method to check if list is empty
+		public boolean isEmpty() {
+			return person.isEmpty();
+		}
+	
+	//Method to check all contacts available 
+	public ArrayList<PersonInfo> viewAllContacts(){
+		return person;
+	}	
+	
+	//Method to modify details
+     	public void Modify(String name,int info_name){
+    		for(int i = 0; i < person.size(); i++){
+             	PersonInfo p = (PersonInfo)person.get(i);
+             	if(name.equals(p.getFirst_name())){
+            		System.out.println("Modify Details");
+            	 	switch(info_name) {
+            	   	case 1: 
+            		   System.out.println("Enter new First Name");
+            		   p.setFirst_name(obj.next());
+            		   break;
+            	   	case 2: 
+            	   		System.out.println("Enter new Last Name");
+           				p.setLast_name(obj.next());
+           				break;
+            	   	case 3: 
+            	   		System.out.println("Enter new Address");
+           				p.setAddress(obj.next());
+           				break;
+            	   	case 4: 
+            	   		System.out.println("Enter new City");
+           				p.setCity(obj.next());
+           				break;
+            	   	case 5:
+            	   		System.out.println("Enter new State");
+           				p.setState(obj.next());
+           				break;
+            	   	case 6:
+            	   		System.out.println("Enter new Zip Code");
+            	   		p.setZip(obj.next());
+            	   		break;
+            	   	case 7:
+            	   		System.out.println("Enter new Phone Number");
+           				p.setPhno(obj.next());
+           				break;
+            	   	case 8:
+            	   		System.out.println("Enter new Email");
+           				p.setEmail(obj.next());
+           				break;
+            	   }	
+             }
+      } 	
+}
+     	//Method to remove contact details
+    	public void remove(String name){
+            for(int i = 0; i < person.size(); i++){
+            	PersonInfo p = (PersonInfo)person.get(i); 
+            	if(name.equals(p.getFirst_name())){
+            		person.remove(i);
+            		System.out.println("Details of "+name+" removed");
+            	}
+               }
+    		}
 
-	public void addPerson(Directory data) {
-		person.add(data);
-	}
-	
-	public void modifyPerson(String name) {
-		for (int i=0; i< person.size();i++) {
-			
-			Directory data = (Directory)person.get(i);
-			if(name.equals(data.getFirst_Name())) {
-				System.out.println("Which Details to modify? Choose\n1. First Name\n2. Last Name\n3. Address\n4. City\n5. State\n6. Zip Code\n7. Phone Number\n8. EmailId\n9. Exit");
-			int choice  = scan.nextInt();	
-			switch(choice) {
-			
-			case 1:
-				System.out.println("Enter new First Name");
-				data.setFirst_Name(scan.next());
-     		   break;
-     	   	
-			case 2: 
-     	   		System.out.println("Enter new Last Name");
-    				data.setLast_Name(scan.next());
-    				break;
-     	   	case 3: 
-     	   		System.out.println("Enter new Address");
-    				data.setAddress(scan.next());
-    				break;
-     	   	case 4: 
-     	   		System.out.println("Enter new City");
-    				data.setCity_Name(scan.next());
-    				break;
-     	   	case 5:
-     	   		System.out.println("Enter new State");
-    				data.setState(scan.next());
-    				break;
-     	   	case 6:
-     	   		System.out.println("Enter new Zip Code");
-     	   		data.setZip_Number(scan.nextInt());
-     	   		break;
-     	   	case 7:
-     	   		System.out.println("Enter new Phone Number");
-    				data.setPhone_Number(scan.nextLong());
-    				break;
-     	   	case 8:
-     	   		System.out.println("Enter new Email");
-    				data.setEmail_Id(scan.next());
-    				break;
-			}
-			}
-		}
-		
-	}
-	public void deletePerson(String delete) {
-		
-		for(int i=0;i<person.size();i++) {
-			Directory data = (Directory)person.get(i);
-			
-			if(delete.equals(data.getFirst_Name())) {
-				person.remove(i);
-				System.out.println("The Contact with first name "+delete+" has been deleted");
-				
-			}
-			
-		}
-	}
-	
-} 
+}
