322301529827319499dc10807dc1ddfd4192195d
nayan-bansal <nayan.7991@gmail.com>
2020-10-01 01:55:09 +0530

    Added: Address Book with multiple contacts

--- Directory.java
+++ Directory.java
@@ -1,65 +1,63 @@
 package AddressBook;
 
-public class Directory {
+public class Main {
 
 	
-	private String first_Name;
-	private String last_Name;
+	private String first_name;
+	private String last_name;
 	private String address;
-	private String city_Name;
+	private String city;
 	private String state;
-	private int zip_Number;
-	private long phone_Number;
-	private String email_Id;
+	private String zip;
+	private String phone_no;
+	private String email;  
 	
-	public String getFirst_Name() {
-		return first_Name;
+public String getFirst_name() {
+		return first_name;
 	}
-	public void setFirst_Name (String first_Name) {
-		this.first_Name = first_Name;
+public void setFirst_name(String first_name) {
+		this.first_name=first_name;
 	}
-	
-	public String getLast_Name() {
-		return last_Name;
+public String getLast_name() {
+		return last_name;
 	}
-	public void setLast_Name(String last_Name) {
-		this.last_Name = last_Name;
+public void setLast_name(String last_name) {
+		this.last_name=last_name;
 	}
-	
-	public String getAddress() {
+public String getAddress() {
 		return address;
 	}
-	public void setAddress (String address) {
-		this.address = address;
+public void setAddress(String address) {
+		this.address=address;
 	}
-	public String getCity_Name() {
-		return city_Name;
+public String getCity() {
+		return city;
 	}
-	public void setCity_Name(String city_Name) {
-		this.city_Name = city_Name;
+public void setCity(String city) {
+		this.city=city;
 	}
-	public String getState() {
+public String getState() {
 		return state;
 	}
-	public void setState(String state) {
-		this.state = state;
+public void setState(String state) {
+		this.state=state;
 	}
-	public int getZip_Number() {
-		return zip_Number;
+public String getZip() {
+		return zip;
 	}
-	public void setZip_Number(int zip_Number) {
-		this.zip_Number = zip_Number;
+public void setZip(String zip) {
+		this.zip=zip;
 	}
-	public long getPhone_Number() {
-		return phone_Number;
+public String getPhno() {
+		return phone_no;
 	}
-	public void setPhone_Number(long phone_Number) {
-		this.phone_Number = phone_Number;
+public void setPhno(String phone_no) {
+		this.phone_no=phone_no;
 	}
-	public String getEmail_Id() {
-		return email_Id;
+public String getEmail() {
+		return email;
 	}
-	public void setEmail_Id(String email_Id) {
-		this.email_Id = email_Id;
+public void setEmail(String email) {
+		this.email=email;
 	}
 }
