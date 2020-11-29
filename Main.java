8f0498b6c84c939b53eca1cec4f2fa5c8a4dfee1
nayan-bansal <69717938+nayan-bansal@users.noreply.github.com>
2020-11-29 13:48:57 +0530

    Updated Main File

--- Main.java
+++ Main.java
@@ -1,17 +1,19 @@
 package AddressBook;
 
-public class Main {
+import java.util.*;
+
+public class Main{
 
 	
-	private TreeMap<String, PersonInfo> addressBook=new TreeMap<String, PersonInfo>();
-	public TreeMap<String, PersonInfo> getAddressBookDict(){
+	private TreeMap<String, Directory> addressBook=new TreeMap<String, Directory>();
+	public TreeMap<String, Directory> getAddressBookDict(){
 		return addressBook;
 	}
-	public void setAddressBookDict(TreeMap<String, PersonInfo> addressBook){
+	public void setAddressBookDict(TreeMap<String, Directory> addressBook){
 		this.addressBook=addressBook;
 	}
 	
-	public void addAddressBook(String AddressBookName,PersonInfo obj) {
+	public void addAddressBook(String AddressBookName,Directory obj) {
 		addressBook.put(AddressBookName,obj);
 	}
 	
